1766293c4fd13528aab5d87724d6ac99f298593d
Saiful Islam <https://github.com/Ferrari2003>
2022-11-08 07:35:12 +0600

    aded commit

--- src/Pages/Shared/About/About.js
+++ src/Pages/Shared/About/About.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import img1 from '../../../images/38bcf751-c8f5-4bce-ac9a-c7c83d5e3cd8.webp'
+import img2 from '../../../images/maxresdefault.jpg'
+import img3 from '../../../images/download.jpeg'
+import img4 from '../../../images/1390921-fire-haircut-1123.webp'
 
 const About = () => {
     return (
         <div>
-            <h1>Hello </h1>
+            <div className="carousel w-full mb-6">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={img1} alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={img2} alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={img3} alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src={img4} alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+            <div className='m-7'>
+                <h1>:Barbers are a specific kind of hairstylist or cosmetologist professional that primarily treats male clientele. They cut, trim, shampoo and condition hair. They may also style hair using hot styling tools and hairstyling products. Some barbers are licensed to color or highlight hair or chemically alter hair textures.</h1>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,15 +7,17 @@ const Header = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <img className='w-40' src={logo} alt="" />
+                <Link to={'/'}>
+                    <img className='w-40' src={logo} alt="" />
+                </Link>
             </div>
             <div className="flex-none gap-2">
-                 <div className='mr-11'>
+                <div className='mr-11'>
                     <Link to={'/'} className='btn btn-outline mr-4'>Home</Link>
                     <Link to={'/about'} className='btn btn-outline btn-primary mr-4'>About</Link>
                     <Link to={'/login'} className='btn btn-outline btn-secondary mr-4'>Login</Link>
                     <Link to={'blog'} className='btn btn-outline btn-accent mr-4'>Blog</Link>
-                 </div>
+                </div>
                 <div className="dropdown dropdown-end mr-7">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
@@ -29,7 +31,7 @@ const Header = () => {
                                 <span className="badge">New</span>
                             </a>
                         </li>
-                        
+
                         <li><a>Logout</a></li>
                     </ul>
                 </div>
