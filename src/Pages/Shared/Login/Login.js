d9126fac2d8fba2837fbbe6b0d706785b0a05b1e
Saiful Islam <https://github.com/Ferrari2003>
2022-11-10 21:13:59 +0600

    last add commit

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -20,8 +20,30 @@ const Login = () => {
         createUser(email, password)
         .then(result => {
             const user = result.user;
-            console.log(user)
-            navigate(from, {replace: true})
+           
+
+            const currentUser = {
+                email: user.email
+            }
+
+            console.log(currentUser)
+            // jwt token
+
+            fetch('http://localhost:5000/jwt',{
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                localStorage.setItem('barber-token',data.token);
+            })
+
+
+            // navigate(from, {replace: true})
         })
         .catch(error => console.error(error));
         
